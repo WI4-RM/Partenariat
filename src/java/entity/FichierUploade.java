7720011172acaa4618cadf127593b29bbffbdd30
Erwan Godefroy <erwan.godefroy@yahoo.fr>
2012-01-23 19:26:55 +0100

    connection fix

--- src/java/entity/FichierUploade.java
+++ src/java/entity/FichierUploade.java
@@ -1,7 +1,7 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 package entity;
 
@@ -22,9 +22,9 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 /**
- *
- * @author lolo
- */
+*
+* @author lolo
+*/
 @Entity
 @Table(name = "fichier_uploade")
 @NamedQueries({
